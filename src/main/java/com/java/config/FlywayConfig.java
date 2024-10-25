e410e0721222e4011831d6a79f21a763646a06d9
Hieu Nguyen Trung (Software Engineer - Technology Division) <hieu.nt60@shbfinance.com.vn>
2024-10-25 16:07:49 +0700

    tich hop flyway voi nhieu DB

---
+++ src/main/java/com/java/config/FlywayConfig.java
@@ -0,0 +1,24 @@
+package com.java.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author hieu.nt60
+ * 10/25/2024
+ */
+@Configuration
+public class FlywayConfig {
+    @Bean(name = "flywayDbLocal")
+    public Flyway flywayDb1(@Qualifier("localDataSource") DataSource localDataSource) {
+        return Flyway.configure()
+                .dataSource(localDataSource)
+                .locations("classpath:db/migration/dblocal")
+                .baselineOnMigrate(true)
+                .load();
+    }
+}
